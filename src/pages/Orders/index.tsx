a35fe10438f2441539cd4ecee27ac014c81711d1
andersonsaj <andersonsb09@hotmail.com>
2021-02-08 15:27:28 -0300

    finalized page orders, integration with api

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+import ProductList from "./ProductsList";
 import StepsHeader from "./StepsHeader"
 import './styles.css';
+import { Product } from "./types";
 
 const Orders: React.FC = () => {
- return (
-     <div className="orders-container">
-        <StepsHeader />
-     </div>
- )
+    const [products, setProducts] = useState<Product[]>([]);
+    
+    useEffect(() => {
+        api.get('/products')
+        .then(response => setProducts(response.data))
+        .catch(error => console.error(error));
+    },[]);
+      
+    return (
+        <div className="orders-container">
+            <StepsHeader />
+            <ProductList products={products} />
+        </div>
+    )
 }
 
 export default Orders;
